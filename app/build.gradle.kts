4f2334d773be014ac26e4755ce757f8d4a014aee
throwawayt2644-arch <throwawayt2644@gmail.com>
2026-05-17 21:43:48 +0530

    Update build.gradle.kts

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -34,6 +34,7 @@ signingConfigs {
     buildTypes {
         release {
             isMinifyEnabled = false
+signingConfig = signingConfigs.getByName("release")
             proguardFiles(
                 getDefaultProguardFile("proguard-android-optimize.txt"),
                 "proguard-rules.pro"
